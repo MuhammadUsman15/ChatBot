4b1862f7996cfc687c0914f7a4aadefc7631ad5a
MuhammadUsman15 <80236188+MuhammadUsman15@users.noreply.github.com>
2024-03-01 22:11:42 +0500

    Update

--- chatbot/chat/views.py
+++ chatbot/chat/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
 
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.completions.create(
+         model = "davinci-002",
+        messages=[
+            {"role": "system", "content": "You are an helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+    )
+    print(response)
+    answer = response.choices[0].message.content.strip()
+    return answer 
 # Create your views here.
 
 def chat(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+        return JsonResponse({'message': message, 'response': response})
     return render (request, 'chatbot.html')
